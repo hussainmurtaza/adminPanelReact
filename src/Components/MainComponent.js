f34276d8be2227c3d43b371ef3304603628285df
MusavirAliJ <musavir@integrity.pk>
2020-09-28 17:09:36 +0500

    routes added

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { Switch, Redirect, Route } from "react-router-dom";
 import Login from "Components/Auth/LoginComponent";
+
 class Main extends Component {
     render() {
-        return <Login />;
+        return (
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Redirect to="/" />
+            </Switch>
+        );
     }
 }
 
