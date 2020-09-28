e82d8d89768dd7d01abcb1f1f9c80a07f9272962
MusavirAliJ <musavir@integrity.pk>
2020-09-28 18:34:37 +0500

    dashboard component created

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Switch, Redirect, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import Login from "Components/Auth/LoginComponent";
+import Dashboard from "Components/Dashboard/DashboardComponent";
 
 class Main extends Component {
     render() {
@@ -9,6 +11,7 @@ class Main extends Component {
             <React.Fragment>
                 <Switch>
                     <Route path="/login" component={Login} />
+                    <Route path="/dashboard" component={Dashboard} />
                     <Redirect to="/" />
                 </Switch>
                 <ToastContainer />
@@ -17,4 +20,10 @@ class Main extends Component {
     }
 }
 
-export default Main;
+const mapStateToProps = (state) => {
+    return {
+        Auth: state.Login,
+    };
+};
+
+export default connect(mapStateToProps)(Main);
