5f839562b475464c3b82c69b4d9029ce06601f28
MusavirAliJ <musavir@integrity.pk>
2020-09-28 17:46:37 +0500

    login service created

---
+++ src/Services/V1/AuthService.js
@@ -0,0 +1,20 @@
+import Gateway from "Gateways/Gateway";
+import V1 from "Constants/V1ApiConstant";
+
+const loginPost=(data) =>{
+    const _data = loginPostBody(data);
+    const response = await Gateway.guestGateway("POST", V1.auth.login, _data);
+    return response;
+}
+
+const loginPostBody = (data) => {
+    let _data = {};
+    _data.email = data.email;
+    _data.password = data.password;
+    return JSON.stringify(_data);
+};
+
+const AuthService = {
+    loginPost,
+};
+export default AuthService;
