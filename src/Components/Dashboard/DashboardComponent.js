0504fcf7d0d89b335b44f248f1dc77e3a72e7aa0
Muhammad Fahim <fahim@integrity.pk>
2020-10-28 17:59:23 +0500

    dashboard ui

--- src/Components/Dashboard/DashboardComponent.js
+++ src/Components/Dashboard/DashboardComponent.js
@@ -60,45 +60,45 @@ class DashboardComponent extends Component {
 									</Card>
 								</Col>
 							</Row>
-							<div className="row tx-14">
+							<div className="row">
 								<div className="col-sm-3">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="users" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="users" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Users</h5>
 										<a href="users" className="tx-medium">View Users <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
 								</div>
 								<div className="col-sm-3 mg-t-20 mg-sm-t-0">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="globe" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="globe" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Roles</h5>
 										<a href="roles" className="tx-medium">View Roles <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
 								</div>
 								<div className="col-sm-3 mg-t-20 mg-sm-t-0">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="user" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="user" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Customers</h5>
 										<a href="customers" className="tx-medium">View Customers <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
 								</div>
 								<div className="col-sm-3 mg-t-20 mg-sm-t-0">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="layers" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="layers" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Sites</h5>
 										<a href="sites" className="tx-medium">View Sites <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
 								</div>
 								<div className="col-sm-3 mg-t-20 mg-sm-t-25">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="inbox" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="inbox" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Invoices</h5>
 										<a href="invoices" className="tx-medium">View Invoices <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
 								</div>
 								<div className="col-sm-3 mg-t-20 mg-sm-t-25">
 									<div className="bg-white bd pd-20">
-										<div className="mg-r-25 float-left"><i data-feather="grid" className="wd-50 ht-50 tx-gray-500"></i></div>
+										<div className="mg-r-10 float-left"><i data-feather="grid" className="wd-50 ht-50 tx-gray-500"></i></div>
 										<h5>Migrations</h5>
 										<a href="migrations" className="tx-medium">View Migrations <i className="icon ion-md-arrow-forward mg-l-5"></i></a>
 									</div>
