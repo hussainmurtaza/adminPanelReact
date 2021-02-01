ffb115d98d2e69e29e9077a44f9caeeff0c34bec
MusavirAliJ <musavir@integrity.pk>
2021-02-01 11:45:47 +0500

    addon issues solved

--- src/Redux/V1/Sites/Details/First/SiteFirstSaga.js
+++ src/Redux/V1/Sites/Details/First/SiteFirstSaga.js
@@ -4,20 +4,18 @@ import SiteFirstAction from "Redux/V1/Sites/Details/First/SiteFirstAction";
 import SiteService from "Services/V1/SiteService";
 
 function* siteDetails(data) {
-	try {
-		const response = yield SiteService.get(data.request);
-		if (response.success) {
-			yield put(SiteFirstAction.siteFirstSuccess(response.data));
-		} else {
-			yield put(
-				SiteFirstAction.siteFirstFailed(response.error.message)
-			);
-		}
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        const response = yield SiteService.get(data.request);
+        if (response.success) {
+            yield put(SiteFirstAction.siteFirstSuccess(response.data));
+        } else {
+            yield put(SiteFirstAction.siteFirstFailed(response.error.message));
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 export function* SiteFirstSaga() {
-	yield takeEvery(SITE.SITE_FIRST, siteDetails);
+    yield takeEvery(SITE.SITE_FIRST, siteDetails);
 }
